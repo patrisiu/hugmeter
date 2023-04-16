772fce64b0eeee54b56e514ae22d039ad83e650b
patrici <patrici.duenas@edreamsodigeo.com>
2023-04-17 01:45:11 +0200

    HugMeter Web Version

--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -12,7 +12,7 @@ const RESOURCES = {
 "assets/NOTICES": "beec41fe8d50122af605843fd87a57af",
 "assets/icons/HugMeter.png": "9e03b39e90cbf7506008eefa7a504427",
 "assets/icons/icon.png": "c6351188d8fcd99eeacaec27eda6ab0b",
-"main.dart.js": "57e8afb69f9f1f1d1b65717ab1b83278",
+"main.dart.js": "c92d11468276cf9b1414dd6b156c1bb9",
 "canvaskit/canvaskit.js": "97937cb4c2c2073c968525a3e08c86a3",
 "canvaskit/canvaskit.wasm": "3de12d898ec208a5f31362cc00f09b9e",
 "canvaskit/profiling/canvaskit.js": "c21852696bc1cc82e8894d851c01921a",
@@ -23,8 +23,8 @@ const RESOURCES = {
 "version.json": "42d6981c66386402c73403fc69b969a6",
 "favicon.png": "3cfead08706941cef16b511bdf8cfe62",
 "flutter.js": "a85fcf6324d3c4d3ae3be1ae4931e9c5",
-"index.html": "0f925315a33fa7b827ac132dc5f1b7e2",
-"/": "0f925315a33fa7b827ac132dc5f1b7e2"
+"index.html": "795ac123a86128b6a12c84c4c73c8591",
+"/": "795ac123a86128b6a12c84c4c73c8591"
 };
 
 // The application shell files that are downloaded before a service worker can
