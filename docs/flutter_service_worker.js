624453a7bb5e375f6dfd11c8d546df94cfe31ba3
Patrici Duenas <patrisiu@gmail.com>
2025-04-30 14:13:19 +0200

    Update Web Version

--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -4,11 +4,11 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
 const RESOURCES = {"flutter.js": "76f08d47ff9f5715220992f993002504",
-"flutter_bootstrap.js": "0c02dcf632511e4d12aab414e35d6c83",
+"flutter_bootstrap.js": "93d68182ded5219f15b78dca0f2ff7e1",
 "manifest.json": "7e5a28255187b3245e0df45ad3c368ce",
 "version.json": "731d469bb432b331dc8b1bbdd8f6054e",
 "favicon.png": "3cfead08706941cef16b511bdf8cfe62",
-"main.dart.js": "f263a127902477b1c9fcec8dd4700a43",
+"main.dart.js": "40c87f19fe3fc81fb3f40b5c7962444b",
 "canvaskit/chromium/canvaskit.js.symbols": "5a23598a2a8efd18ec3b60de5d28af8f",
 "canvaskit/chromium/canvaskit.wasm": "64a386c87532ae52ae041d18a32a3635",
 "canvaskit/chromium/canvaskit.js": "ba4a8ae1a65ff3ad81c6818fd47e348b",
@@ -33,8 +33,8 @@ const RESOURCES = {"flutter.js": "76f08d47ff9f5715220992f993002504",
 "assets/AssetManifest.bin.json": "ac46cabdb09eafba4457f9c49f602ae5",
 "assets/AssetManifest.json": "d31fcfb0405d09875306d3589ea8af65",
 "assets/shaders/ink_sparkle.frag": "ecc85a2e95f5e9f53123dcaf8cb9b6ce",
-"index.html": "316e6b4971febb5416e54da71301bfa7",
-"/": "316e6b4971febb5416e54da71301bfa7",
+"index.html": "56338d52dd90731b65734105073c6a8e",
+"/": "56338d52dd90731b65734105073c6a8e",
 "icons/HugMeter-192.png": "0c71dbda12ae44a1b0388c6abd71a777",
 "icons/HugMeter-512.png": "9e03b39e90cbf7506008eefa7a504427"};
 // The application shell files that are downloaded before a service worker can
