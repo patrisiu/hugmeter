0462f9d46ed2b5856a80a42c2bb000af0b4e4f89
Patrici Duenas <patrisiu@gmail.com>
2025-06-08 11:17:53 +0200

    Update Web Version

--- docs/flutter_bootstrap.js
+++ docs/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"f73bfc4522dd0bc87bbcdb4bb3088082755c5e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2680166071"
+    serviceWorkerVersion: "279356802"
   }
 });
