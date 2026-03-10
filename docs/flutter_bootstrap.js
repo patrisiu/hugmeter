64f3450aec959d17978f010dbda54edebb02a03b
Patrici <patrisiu@gmail.com>
2026-03-10 14:27:28 +0100

    Web investigation

--- docs/flutter_bootstrap.js
+++ docs/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"80f576e2690e0366991d5dcd648d6cbe1ca464
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1578196800" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1163110880" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
